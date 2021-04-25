c51a533308ca5c567c3e1e7a919bd1872749b280
cracko <lukashais@getgoogleoff.me>
2021-04-25 11:03:49 +0200

    dir rename

---
+++ ipk-sniffer/IPK-packet-sniffer/Options.cs
@@ -0,0 +1,58 @@
+using CommandLine;
+
+namespace IPK_packet_sniffer
+{
+  /// <summary>
+  /// Class containing options for parsing arguments from commandline
+  /// </summary>
+  public class Options
+  {
+    [Option(
+      'i',
+      "interface",
+      HelpText = "Lists all available devices",
+      Default = "None"
+    )]
+    public string Interface { get; set; }
+
+    [Option(
+      'p',
+      HelpText = "Filters only packets on this port",
+      Default = null 
+    )]
+    public int? PortNumber { get; set; }
+
+    [Option(
+      't',
+      "tcp",
+      HelpText = "Filters that packets are of TCP type"
+    )]
+    public bool TcpOnly { get; set; }
+
+    [Option(
+      'u',
+      "udp",
+      HelpText = "Filters that packets are of UDP type"
+    )]
+    public bool UdpOnly { get; set; }
+
+    [Option(
+      "arp",
+      HelpText = "Filters that packets are of ARP type"
+    )]
+    public bool ArpOnly { get; set; }
+
+    [Option(
+      "icmp",
+      HelpText = "Filters that packets are of ICMP type"
+    )]
+    public bool IcmpOnly { get; set; }
+
+    [Option(
+      'n',
+      HelpText = "Expected number of packets to catch",
+      Default = 1
+    )]
+    public int NumberOfPackets { get; set; }
+  }
+}
---
+++ ipk-sniffer/IPK-packet-sniffer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace IPK_packet_sniffer
+{
+  internal static class Program
+  {
+    private static void Main(string[] args)
+    {
+      // parsing of commandline arguments
+      var parser = new Parser();
+      parser.ParseArguments<Options>(args)
+        .WithParsed((o) =>
+        {
+          // if no device was selected to listen for -> list all devices
+          if (o.Interface.Equals("None"))
+          {
+            Sniffer.ListAvailableDevices();
+          }
+          // if port is not null and not in valid range
+          else if (o.PortNumber != null && (o.PortNumber > 65535 || o.PortNumber < 1))
+          {
+            Console.WriteLine("Port number is not in <1, 65535> range, exiting...");
+            Environment.Exit(ReturnCodes.InvalidPortNumber);
+          }
+          // start sniffing
+          else
+          {
+            Sniffer.SniffPackets(o);
+            Environment.Exit(ReturnCodes.Success);
+          }
+        })
+        .WithNotParsed(ErrorHandler);
+    }
+
+    /// <summary>
+    /// Handles errors when parsing arguments
+    /// </summary>
+    /// <param name="errors">Enumerable containing Errors</param>
+    private static void ErrorHandler(IEnumerable<Error> errors)
+    {
+      var tmpError = "Encountered following error(s) when parsing arguments\n";
+      foreach (var e in errors)
+      {
+        if (e is MissingValueOptionError valErr)
+        {
+          // if missing value for -i or --interface 
+          if (valErr.NameInfo.ShortName == "i")
+            Sniffer.ListAvailableDevices();
+          // otherwise list parameters that have missing value
+          else
+          {
+            tmpError += "\tparam: " +
+                        valErr.NameInfo.NameText +
+                        " -> " +
+                        valErr.Tag +
+                        "\n";
+          }
+        }
+        else
+          tmpError += "\t" + e.Tag + "\n";
+      }
+
+      Console.WriteLine(tmpError);
+      Environment.Exit(ReturnCodes.InvalidArguments);
+    }
+  }
+}
